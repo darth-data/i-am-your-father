694af6b2e842c5ebdeac16be66304bbb30e2eff4
Chris Leonard <chris@databaseguy.com>
2024-12-15 13:33:35 -0600

    initial commit

---
+++ hello.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+BREATH_RATE=4
+BREATH_NOISE="kshhhuuuuuuuuuhhh"
+
+sleep $BREATH_RATE
+echo $BREATH_NOISE
+sleep $BREATH_RATE
+echo $BREATH_NOISE
+sleep $BREATH_RATE
+echo "$USER, I am your father."
+sleep $BREATH_RATE
+echo $BREATH_NOISE
+sleep $BREATH_RATE
+echo $BREATH_NOISE
+sleep $BREATH_RATE
+
